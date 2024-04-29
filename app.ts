d1c9ac91fa1854b407de6d4bb020d1e8d8b3fd06
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 20:55:56 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+let totalMarks: number = 850;
+let obtainMarks: number = 620;
+let per: number = (obtainMarks / totalMarks) * 100;
+console.log(per);
+
+if (per >= 90 && per <= 100) {
+    console.log(`your per is ${per} which is A+ garde and your are eligible fo the next class`)
+} else if (per >= 80 && per <= 89) {
+    console.log(`your per is ${per} which is A garde and your are eligible fo the next class`)
+} else if (per >= 70 && per < 79) {
+    console.log(`your per is ${per} which is B garde and your are eligible fo the next class`)
+} else if (per >= 60 && per < 69) {
+    console.log(`your per is ${per} which is C garde and your are eligible fo the next class`)
+}
+else {
+    console.log(`your per is ${per} which is D garde and below expectation and your are not eligible fo the next class`)
+}
